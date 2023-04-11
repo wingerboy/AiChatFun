57abce755ed702bd0bac6d3d25f095a6b306a719
zhanpengyu <zhanpengyu@tp-link.com.cn>
2023-04-11 08:48:46 +0800

    修改host文件用于线上页面请求

--- vue/src/utils/host.js
+++ vue/src/utils/host.js
@@ -1,6 +1,7 @@
 const apiHostMap = {
-    qa: 'http://localhost:7000',
-    prod: 'http://43.153.81.198:8000',
-}
+  qa: "http://localhost:7000",
+  prod: "http://43.153.81.198:8000",
+  online: "http://43.153.81.198",
+};
 
-export default apiHostMap['qa'];
+export default apiHostMap["online"];
